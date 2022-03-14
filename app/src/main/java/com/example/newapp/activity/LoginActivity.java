e8666f2c4dc2c7a0fb6ab737f1b60b320f781c64
sariamanda <12050123368@students.uin-suska.ac.id>
2022-03-14 13:35:56 +0700

    1

---
+++ app/src/main/java/com/example/newapp/activity/LoginActivity.java
@@ -0,0 +1,46 @@
+package com.example.newapp.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.newapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    ImageButton login;
+    EditText username, password;
+
+    protected void onCreate (Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+
+        setContentView(R.layout.login_relative);
+
+        login = (ImageButton) findViewById(R.id.btnLogin);
+        username = (EditText) findViewById(R.id.etUsername);
+        password = (EditText) findViewById(R.id.etPassword);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (password.getText().toString().toUpperCase().equals("TEST1")) {
+                    login_sukses();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Salah Password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void login_sukses() {
+        String user_login = username.getText().toString();
+        Intent i = new Intent(this, MainActivity.class);
+
+        //kirimkan usernamenya ke tampilan utama
+        i.putExtra("USERNAME", user_login);
+        startActivity(i);
+    }
+}
